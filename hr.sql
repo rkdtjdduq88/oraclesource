e015b584742d56e521e1f08e45284121cf0d06ed
seungyeop <rkdtjdduq88@naver.com>
2023-03-21 17:49:57 +0900

    select 함수 작성

--- hr.sql
+++ hr.sql
@@ -12,104 +12,266 @@ FROM
     employees;
     
 -- 사원번호가 176인 사람의 LAST_NAME 조회
-SELECT LAST_NAME
-FROM employees
-WHERE employee_id = 176;
+SELECT
+    last_name
+FROM
+    employees
+WHERE
+    employee_id = 176;
 
 -- 연봉이 12000 이상 되는 직원들의 LAST_NAME, SALARAY 조회
-SELECT last_name, salary
-FROM employees
-WHERE salary >= 12000;
+SELECT
+    last_name,
+    salary
+FROM
+    employees
+WHERE
+    salary >= 12000;
 
 -- 연봉이 5000 에서 12000 범위가 아닌 사람들의 LAST_NAME, SALARAY 조회
-SELECT last_name, salary
-FROM employees
-WHERE  salary < 5000 OR salary > 12000 ORDER BY last_name DESC;
+SELECT
+    last_name,
+    salary
+FROM
+    employees
+WHERE
+    salary < 5000
+    OR salary > 12000
+ORDER BY
+    last_name DESC;
 
 
 -- 20, 50 번 부서에서 근무하는 모든 사원들의 LAST_NAME, 부서번호를 오름차순으로 조회
-SELECT LAST_NAME,DEPARTMENT_ID
-FROM employees
-WHERE DEPARTMENT_ID IN(20,50) ORDER BY LAST_NAME,DEPARTMENT_ID;
+SELECT
+    last_name,
+    department_id
+FROM
+    employees
+WHERE
+    department_id IN ( 20, 50 )
+ORDER BY
+    last_name,
+    department_id;
 
 -- 커미션을 받는 모든 사원들의 LAST_NAME, 연봉, 커미션 조회(연봉의 내림차순, 커미션 내림차순)
-SELECT last_name, salary, commission_pct
-FROM employees
-WHERE COMMISSION_PCT > 0 ORDER BY salary DESC, commission_pct DESC;
+SELECT
+    last_name,
+    salary,
+    commission_pct
+FROM
+    employees
+WHERE
+    commission_pct > 0
+ORDER BY
+    salary DESC,
+    commission_pct DESC;
 
 -- 연봉이 2500,3500,7000이 아니며 직업이 SA_REP 나 ST_CLERK 인 사원 조회
-SELECT *
-FROM employees
-WHERE salary NOT IN(2500,3500,7000) AND job_id IN('SA_REP','ST_CLERK');
+SELECT
+    *
+FROM
+    employees
+WHERE
+    salary NOT IN ( 2500, 3500, 7000 )
+    AND job_id IN ( 'SA_REP', 'ST_CLERK' );
 
 -- 2008/02/20 ~ 2008/05/01 사이에 고용된 사원들의 last_name,사번,고용일자 조회
 -- 고용일자 내림차순 정렬
 -- 날짜 표현시 홑따옴표 안에 표현 - or /  사용 가능
-SELECT last_name,employee_id,hire_date
-FROM employees
-WHERE hire_date >= '2008-02-20' AND hire_date <= '2008-05-01' ORDER BY hire_date DESC;
+SELECT
+    last_name,
+    employee_id,
+    hire_date
+FROM
+    employees
+WHERE
+        hire_date >= '2008-02-20'
+    AND hire_date <= '2008-05-01'
+ORDER BY
+    hire_date DESC;
 
-SELECT last_name,employee_id,hire_date
-FROM employees
-WHERE hire_date BETWEEN '2008-02-20' AND '2008-05-01' ORDER BY hire_date DESC;
+SELECT
+    last_name,
+    employee_id,
+    hire_date
+FROM
+    employees
+WHERE
+    hire_date BETWEEN '2008-02-20' AND '2008-05-01'
+ORDER BY
+    hire_date DESC;
 
 -- 2004년도에 고용된 사원들의 last_name, hire_date 조회
 -- 고용일자 오름차순 정렬
-SELECT last_name, hire_date
-FROM employees
-WHERE hire_date >='2004-01-01' AND hire_date <='2004-12-31' ORDER BY hire_date ASC;
+SELECT
+    last_name,
+    hire_date
+FROM
+    employees
+WHERE
+        hire_date >= '2004-01-01'
+    AND hire_date <= '2004-12-31'
+ORDER BY
+    hire_date ASC;
 
 -- 부서가 20,50이고 연봉이 5000에서 12000 범위인 사람들의 last_name, salary 조회
 -- 연봉 오름차순 정렬
-SELECT last_name,salary
-FROM employees
-WHERE department_id in(20,50) AND salary BETWEEN 5000 and 12000 ORDER BY salary;
+SELECT
+    last_name,
+    salary
+FROM
+    employees
+WHERE
+    department_id IN ( 20, 50 )
+    AND salary BETWEEN 5000 AND 12000
+ORDER BY
+    salary;
 
 -- LIKE
 -- 2004년도 고용된 사원들의 last_name, hire_date 조회
 -- 고용일자 오름차순 정렬
-SELECT last_name, hire_date
-FROM employees
-WHERE hire_date LIKE '04%' ORDER BY hire_date;
+SELECT
+    last_name,
+    hire_date
+FROM
+    employees
+WHERE
+    hire_date LIKE '04%'
+ORDER BY
+    hire_date;
 
 -- LAST_NAME 에 U가 포함된 사원들의 사번, last_name 조회
-SELECT employee_id,last_name
-FROM employees
-WHERE last_name LIKE '%U%';
+SELECT
+    employee_id,
+    last_name
+FROM
+    employees
+WHERE
+    last_name LIKE '%U%';
 
 -- LAST_NAME 에 네번째 글자가 a인 사원들의 last_name 조회
-SELECT last_name
-FROM employees
-WHERE last_name LIKE '___a%';
+SELECT
+    last_name
+FROM
+    employees
+WHERE
+    last_name LIKE '___a%';
 
 -- LAST_NAME 에 a 혹은 e 글자가 포함된 사원들의 last_name 조회
 -- last_name 오름차순 정렬
-SELECT last_name
-FROM employees
-WHERE last_name LIKE '%a%' OR last_name LIKE '%e%' ORDER BY last_name;
+SELECT
+    last_name
+FROM
+    employees
+WHERE
+    last_name LIKE '%a%'
+    OR last_name LIKE '%e%'
+ORDER BY
+    last_name;
 
 -- LAST_NAME 에 a 와 e 글자가 포함된 사원들의 last_name 조회
 -- last_name 오름차순 정렬
-SELECT last_name
-FROM employees
-WHERE last_name LIKE '%a%e%' AND last_name LIKE '%e%a%' ORDER BY last_name;
+SELECT
+    last_name
+FROM
+    employees
+WHERE
+    last_name LIKE '%a%e%'
+    AND last_name LIKE '%e%a%'
+ORDER BY
+    last_name;
 
 -- 매니저(manager_id)가 없는 사람들의 last_name, job_id 조회
-SELECT last_name, job_id
-FROM employees
-WHERE manager_id is null OR manager_id = '';
+SELECT
+    last_name,
+    job_id
+FROM
+    employees
+WHERE
+    manager_id IS NULL
+    OR manager_id = '';
 
 -- job_id가 ST_CLERK 인 사원의 부서번호 조회(단 부서번호가 NULL인 사원은 제외한다.)
 -- 중복을 제거한 후 부서번호만 조회
-SELECT DISTINCT department_id
-FROM employees
-WHERE job_id = 'ST_CLERK' AND department_id is not null; 
+SELECT DISTINCT
+    department_id
+FROM
+    employees
+WHERE
+        job_id = 'ST_CLERK'
+    AND department_id IS NOT NULL; 
 
 -- commission_pct null이 아닌 사원들 중에서 commission = salary * commission_pct 를 구하여
 -- employee_id, first_name, job_id 출력
-SELECT employee_id, first_name, job_id, commission_pct * salary as commission
+SELECT
+    employee_id,
+    first_name,
+    job_id,
+    commission_pct * salary AS commission
+FROM
+    employees
+WHERE
+    commission_pct IS NOT NULL;
+    
+-- first_name 이 Curtis 인 사람의 first_name, last_name, email, phone_number, job_id를 조회한다.
+-- 단, job_id 의 결과는 소문자로 출력되도록 한다.
+SELECT first_name, last_name, email, phone_number, LOWER(job_id)
 FROM employees
-WHERE commission_pct is not null;
+WHERE first_name = 'Curtis';
+
+-- 부서번호가 60,70,80,90인 사원들의 employee_id, first_name, hire_date, job_id 조회
+-- 단, job_id가 IT_PROG 인 사원의 경우 프로그래머로 변경한 후 출력
+SELECT employee_id, first_name, hire_date, replace(job_id,'IT_PROG','프로그래머')
+FROM employees
+WHERE department_id in(60,70,80,90);
+
+-- job_id가 AD_PRES, PU_CLERK 인 사원들의 employee_id, first_name, last_name, department_id, job_id를 조회한다.
+-- 단 사원명은 first_name 과 last_name을 연결하여 출력하시오
+SELECT employee_id, concat(first_name,concat(' ',last_name)), department_id, job_id
+FROM employees   -- first_name || ' ' || last_name 도 됨
+WHERE job_id in('AD_PRES','PU_CLERK');
+
+--[실습]SQL 작성
+SELECT last_name, salary, 
+decode(trunc(salary / 2000, 0),
+        0,
+        0.00,
+        1,
+        0.09,
+        2,
+        0.20,
+        3,
+        0.30,
+        4,
+        0.40,
+        5,
+        0.42,
+        6,
+        0.44,
+        0.45) AS tax_rate
+FROM employees
+WHERE department_id = 80;
 
 
+SELECT last_name,salary,
+case 
+    when salary between 0 and 1999 then '  0.00'
+    when salary between 2000 and 3999 then '  0.09'
+    when salary between 4000 and 5999 then '  0.2'
+    when salary between 6000 and 7999 then '  0.3'
+    when salary between 8000 and 9999 then '  0.4'
+    when salary between 10000 and 11999 then '  0.42'
+    when salary between 12000 and 13999 then '  0.44'
+    else '  0.45'
+    end as tax_rate
+FROM employees
+WHERE department_id = 80;
+
+-- 회사 내의 최대 연봉 및 최소 연봉의 차이를 출력
+SELECT max(salary)-min(salary) as sal_gap
+FROM employees;
+
+-- 매니저로 근무하는 사원들의 총 숫자 출력(매니저 중복 제거)
+SELECT count(distinct manager_id)
+FROM employees;
 
--- scott.sql
+++ scott.sql
@@ -683,3 +683,363 @@ FROM emp;
 -- '  ORACLE' = 'ORACLE' ==> FALSE
 SELECT '         이것이       ',trim('         이것이       ')
 FROM dual; 
+
+
+-- 2. 숫자함수
+-- 1) ROUND, TRUNC, CEIL, FLOOR, MOD
+
+-- round : 반올림
+-- round(숫자, 반올림 위치(선택))
+SELECT round(1234.5678)    as round,       -- 소수점 첫째 자리에서 반올림
+       round(1234.5678,0)  as round0,      -- 소수점 첫째 자리에서 반올림
+       round(1234.5678,1)  as round1,      -- 소수점 둘째 자리에서 반올림 
+       round(1234.5678,2)  as round2,      -- 소수점 셋째 자리에서 반올림
+       round(1234.5678,-1) as round_minus1,-- 자연수 첫째 자리에서 반올림
+       round(1234.5678,-2) as round_minus2 -- 자연수 둘째 자리에서 반올림
+FROM
+    dual;
+    
+-- trunc : 특정 위치에서 버리는 함수
+-- trunc(숫자, 버림 위치(선택))
+SELECT trunc(1234.5678)    as trunc,       -- 소수점 첫째 자리에서 버림
+       trunc(1234.5678,0)  as trunc0,      -- 소수점 첫째 자리에서 버림
+       trunc(1234.5678,1)  as trunc1,      -- 소수점 둘째 자리에서 버림 
+       trunc(1234.5678,2)  as trunc2,      -- 소수점 셋째 자리에서 버림
+       trunc(1234.5678,-1) as trunc_minus1,-- 자연수 첫째 자리에서 버림
+       trunc(1234.5678,-2) as trunc_minus2 -- 자연수 둘째 자리에서 버림
+FROM
+    dual;    
+
+-- ceil(숫자), floor(숫자) : 입력된 숫자와 가까운 큰 정수, 작은 정수
+SELECT ceil(3.14), floor(3.14), ceil(-3.14), floor(-3.14)
+FROM dual;
+
+-- mod(숫자, 나눌수) : 나머지값
+SELECT mod(15,6),mod(10,2),mod(11,2)
+FROM dual;
+
+-- 날짜 함수
+-- 날짜 데이터 + 숫자 : 날짜 데이터보다 숫자만큼 일 수 이후의 날짜
+-- 날짜 데이터 - 날짜 데이터 : 두 날짜 데이터 간의 일수 차이
+
+-- 날짜 데이터 + 날짜 데이터 : 연산불가
+
+-- 1) sysdate 함수 : 오라클 데이터베이스 서버가 설치된 OS의 현재날짜와 시간을 가져옴
+SELECT sysdate, sysdate-1 as yesterday, sysdate+1 as tomorrow
+FROM dual;
+
+-- 2) add_months(날짜,더할 개월수) : 몇 개월 이후 날짜 구하기
+SELECT sysdate, add_months(sysdate,3)
+FROM dual;
+
+-- 입사 50주년이 되는 날짜 구하기
+-- empno,ename,hiredate, 입사50주년날짜 조회
+SELECT empno,ename,hiredate,add_months(hiredate,600)
+FROM emp;
+
+-- 3) MONTHS_BETWEEN(첫번째날짜, 두번째 날짜) : 두 날짜 데이터 간의 날짜 차이를 개월수로 계산하여 출력
+-- 입사 45년 미만인 사원 데이터 조회
+-- empno,ename,hiredate
+SELECT empno,ename,hiredate
+FROM emp
+WHERE months_between(sysdate,hiredate) < 540;
+
+-- 현재 날짜와 6개월 후 날짜가 출력
+SELECT sysdate,add_months(sysdate,6)
+FROM dual;
+
+SELECT empno, ename, hiredate, sysdate,
+months_between(hiredate,sysdate) as months1,
+months_between(sysdate,hiredate) as months2,
+trunc(months_between(sysdate,hiredate)) as months3
+FROM emp;
+
+-- 4) next_day(날짜, 요일) : 특정 날짜를 기준으로 돌아오는 요일의 날짜 출력 
+--    last_day(날짜) : 특정 날짜가 속한 달의 마지막 날짜를 출력
+SELECT sysdate, next_day(sysdate,'금요일'),last_day(sysdate)
+FROM dual;
+
+-- 날짜의 반올림, 버림 : ROUND, TRUNC
+-- CC : 네 자리 연도의 끝 두자리를 기준으로 사용
+--    : 2023년인 경우 2050 이하이므로 2001년으로 처리
+SELECT sysdate, 
+       round(sysdate, 'CC') as format_cc,
+       round(sysdate, 'YYYY') as format_YYYY,
+       round(sysdate, 'DDD') as format_DDD, -- 오늘 날짜 기준으로 반올림
+       round(sysdate, 'HH') as format_HH -- 지금 시간 기준으로 반올림
+FROM dual;
+
+-- 형변환 함수 : 자료형을 형 변환
+-- NUMBER, VARCHAR2, DATE
+SELECT empno,ename,empno+'500'
+FROM emp
+WHERE ename = 'FORD';
+
+-- ORA-01722: 수치가 부적합합니다
+--SELECT empno,ename,'abcd'+empno
+--FROM emp
+--WHERE ename = 'FORD';
+
+-- TO_CHAR() : 숫자 또는 날짜 데이터를 문자 데이터로 변환 
+-- TO_NUMBER() : 문자 데이터를 숫자 데이터로 변환
+-- TO_DATE() : 문자 데이터를 날짜 데이터로 변환
+
+-- 원하는 출력 형태로 날짜 출력하기 to_char 주로 사용됨
+SELECT sysdate, to_char(sysdate, 'yyyy/mm/dd hh24:mi:ss') as 현재날짜시간
+FROM dual;
+
+-- MON, MONTH
+-- DDD : 365일 중에서 며칠
+SELECT sysdate, 
+       to_char(sysdate, 'yyyy/mm/dd') as 현재날짜,
+       to_char(sysdate, 'yyyy') as 현재연도,
+       to_char(sysdate, 'mm') as 현재월,
+       to_char(sysdate, 'MON') as 현재월1,
+       to_char(sysdate, 'MONTH') as 현재월2,
+       to_char(sysdate, 'dd') as 현재일자,
+       to_char(sysdate, 'ddd') as 현재일자2,
+       to_char(sysdate, 'hh12:mi:ss am') as 현재날짜시간
+FROM dual;
+
+-- sal 필드 , 나 통화표시를 하고 싶다면?
+-- L (Locale) 지역 화폐 단위 기호 붙여줌
+SELECT sal, to_char(sal, '$999,999') as sal_$, to_char(sal, 'L999,999') as sal_L
+FROM emp;
+
+-- to_number(문자열데이터, 인식될 숫자형태)
+
+-- 자동형변환
+SELECT 1300 - '1500', '1300' + 1500 
+FROM dual;
+
+-- 자동형변환 안되는 상황
+SELECT to_number('1,300','999,999') - to_number('1,500','999,999') 
+FROM dual;
+
+-- to_date(문자열데이터, '인식될 날짜 형태')
+SELECT to_date('2018-07-14' , 'yyyy-mm-dd') as TODATE1,
+       to_date('20230320' , 'yyyy-mm-dd') as TODATE2
+FROM dual;
+
+--ORA-01722: 수치가 부적합합니다
+--SELECT '2023-03-21' - '2023-02-01'
+--FROM dual;
+
+SELECT to_date('2023-03-21') - to_date('2023-02-01')
+FROM dual;
+
+-- 널처리 함수
+-- NULL + 300 => NULL
+
+-- NVL(데이터, 널일 경우 반환할 데이터)
+SELECT empno, ename, sal, comm, sal+comm, nvl(comm,0), sal+nvl(comm,0)
+FROM emp;
+
+-- NVL2(데이터, 널이 아닐 경우 반환할 데이터 , 널일 경우 반환할 데이터)
+SELECT empno, ename, sal, comm, nvl2(comm,'O','X'), nvl2(comm,sal*12+comm,sal*12) AS annsal
+FROM emp;
+
+-- DECODE 함수 / CASE 문
+
+-- DECODE(검사 대상이 될 데이터, 
+--        조건1, 조건1이 일치할때 실행할 구문
+--        조건2, 조건2이 일치할때 실행할 구문
+
+-- emp 테이블에 직책이 manager 인 사람은 급여의 10% 인상, 
+-- salesman 인 사람은 5%, analyst 인 사람은 그대로, 나머지는 3% 인상된 급여 출력
+SELECT empno,ename,job,sal,
+decode(job,'MANAGER',sal*1.1,
+           'SALESMAN',sal*1.05,
+           'ANALYST', sal,
+           sal*1.03) AS upsal
+FROM emp;
+
+
+SELECT empno,ename,job,sal,
+case job
+when'MANAGER' then sal*1.1
+when'SALESMAN' then sal*1.05
+when'ANALYST' then sal
+else sal*1.03 
+end AS upsal
+FROM emp;
+
+-- case 문의 장점
+SELECT empno,ename,job,sal,
+case 
+    when comm is null then '해당사항 없음'
+    when comm = 0 then '수당없음'
+    when comm > 0 then '수당 : ' || comm
+end as comm_text
+FROM emp;
+
+-- [실습]SQL 작성
+-- emp 테이블에서 사원들의 월 평균 근무일수는 21.5일이다. 하루 근무시간을 8시간으로 보았을 때 사원들의 하루 급여(DAY_PAY)와
+-- 시급(TIME_PAY)를 계산하여 결과를 출력한다. 단, 하루 급여는 소수점 셋째 자리에서 버리고, 시급은 두 번째 소수점에서 반올림하시오.
+SELECT empno,ename,sal,trunc(sal/21.5,2) as day_pay,round(sal/21.5/8,1) as time_pay
+FROM emp;
+
+-- [실습]SQL 작성
+-- emp 테이블에서 사원들은 입사일(HIREDATE)을 기준으로 3개월이 지난 후 첫 월요일에 정직원이 된다. 사원들이
+-- 정직원이 되는 날짜(R_JOB)를 YYYY-MM-DD 형식으로 아래와 같이 출력하시오.
+-- 단, 추가수당(COMM)이 없는 사원의 추가 수당은 N/A로 출력하시오.
+SELECT empno,ename,hiredate,next_day(add_months(hiredate,3),'월요일') as r_job,nvl(to_char(comm),'N/A') as comm
+FROM emp;
+
+-- [실습]SQL 작성
+-- [실습3] EMP 테이블의 모든 사원을 대상으로 직속 상관의 사원 번호(MGR)를 다음과 같은 조건을 기준으로 변환해서 CHG_MGR 열에 출력하시오.
+--직속상관의 사원 번호가 존재하지 않을 경우 : 0000
+--직속상관의 사원 번호 앞 두자리가 75일 경우 : 5555
+--직속상관의 사원 번호 앞 두자리가 76일 경우 : 6666
+--직속상관의 사원 번호 앞 두자리가 77일 경우 : 7777
+--직속상관의 사원 번호 앞 두자리가 78일 경우 : 8888
+--그 외 직속 상관 사원 번호의 경우 : 본래 직속 상관의 사원번호 그대로 출력
+SELECT empno,ename,mgr,
+decode(substr(to_char(mgr),1,2), 
+     null, '0000',
+     '75','5555',
+     '76','6666',
+     '77','7777',
+     '78','8888',
+     substr(to_char(mgr),1)
+     ) AS chg_mgr
+FROM emp;
+
+SELECT empno,ename,mgr,
+case
+    when mgr is null then '0000'
+    when substr(to_char(mgr),1,2) = '75' then '5555'
+    when substr(to_char(mgr),1,2) = '76' then '6666'
+    when substr(to_char(mgr),1,2) = '77' then '7777'
+    when substr(to_char(mgr),1,2) = '78' then '8888'
+    else to_char(mgr) end as chr_mgr
+FROM emp;
+
+-- 다중행(집계) 함수 : sum, count, max, min, avg
+
+-- ORA-00937: 단일 그룹의 그룹 함수가 아닙니다
+SELECT SUM(sal)
+FROM emp;
+
+
+
+SELECT SUM(distinct sal), SUM(all sal), SUM(sal)
+FROM emp;
+
+-- sum() : NULL 은 제외하고 합계 구해줌
+SELECT SUM(comm)
+FROM emp;
+
+-- count() : 행 개수
+SELECT COUNT(sal)
+FROM emp;
+
+SELECT COUNT(comm)
+FROM emp;
+
+SELECT COUNT(*)
+FROM emp;
+
+SELECT COUNT(*)
+FROM emp
+WHERE deptno = 30;
+
+-- max() : 최대값 (날짜는 최신일자)
+SELECT MAX(sal)
+FROM emp;
+
+-- 부서번호가 20인 사원의 입사일 중 제일 최근 입사일
+SELECT MAX(hiredate)
+FROM emp
+WHERE deptno = 20;
+
+-- min() : 최소값
+-- 부서번호가 20인 사원의 입사일 중 제일 오래된 입사일
+SELECT MIN(hiredate)
+FROM emp
+WHERE deptno = 20;
+
+-- avg() : 평균
+-- 부서번호가 30인 사원의 평균급여
+SELECT trunc(AVG(sal))
+FROM emp
+WHERE deptno = 30;
+
+-- group by : 결과 값을 원하는 열로 묶어 출력
+
+-- 부서별 sal 평균 구하기
+SELECT AVG(sal) FROM emp WHERE deptno = 10;
+SELECT AVG(sal) FROM emp WHERE deptno = 20;
+SELECT AVG(sal) FROM emp WHERE deptno = 30;
+
+SELECT AVG(sal),deptno
+FROM emp
+GROUP BY deptno;
+
+-- 부서별 추가수당 평균 구하기
+SELECT deptno, avg(comm)
+FROM emp
+GROUP BY deptno;
+
+--ORA-00979: GROUP BY 표현식이 아닙니다.
+--SELECT AVG(sal),deptno,ename
+--FROM emp
+--GROUP BY deptno;
+
+-- GROUP BY + HAVING : group by 절에 조건을 줄 때
+-- HAVING : 그룹화된 대상을 출력 제한 걸때
+
+-- 각 부서의 직책별 평균 급여 구하기(단, 평균 급여가 2000 이상인 그룹만 출력)
+-- deptno, job, 평균
+SELECT deptno, job, avg(sal) 
+FROM emp
+GROUP BY deptno,job having avg(sal) >= 2000
+ORDER BY deptno,job;
+
+-- ORA-00934: 그룹 함수는 허가되지 않습니다
+--SELECT deptno, job, avg(sal) 
+--FROM emp
+--WHERE avg(sal) >= 2000
+--GROUP BY deptno,job
+--ORDER BY deptno,job;
+
+-- 1) from 구문 실행
+-- 2) where 실행
+-- 3) GROUP BY
+-- 4) having
+-- 5) select
+-- 6) order by
+-- emp 테이블에서 연봉이 3000 이하인 사원 중에 평균 연봉이 2000 이상인 사원의 부서별 직책
+SELECT deptno, job, avg(sal) 
+FROM emp
+WHERE sal <= 3000
+GROUP BY deptno,job having avg(sal) >= 2000
+ORDER BY deptno,job;
+
+-- [실습]SQL 작성
+-- [실습1] EMP 테이블을 이용하여 부서번호(DEPTNO), 평균급여(AVG_SAL), 최고급여(MAX_SAL), 최저급여(MIN_SAL), 사원수(CNT)를 출력한다. 
+-- 단, 평균 급여를 출력할 때 소수점을 제외하고 각 부서번호별로 출력하는 SQL 문을 작성하시오.
+SELECT deptno, floor(avg(sal)) as avg_sal, max(sal) as max_sal, min(sal) as min_sal, count(*) as CNT
+FROM emp
+GROUP BY deptno
+ORDER BY deptno DESC;
+
+--[실습2] 같은 직책에 종사하는 사원이 3명 이상인 직책과 인원수를 출력하는 SQL문을 작성하시오.
+SELECT job,count(*)
+FROM emp
+GROUP BY job having count(job) >= 3;
+
+-- [실습3] 사원들의 입사연도(HIRE_YEAR)를 기준으로 부서별로 몇 명이 입사했는지 출력하는 SQL문 작성하시오.
+SELECT to_char(hiredate,'yyyy') as hire_year, deptno, count(*) as cnt
+FROM emp
+GROUP BY deptno,to_char(hiredate,'yyyy');
+
+
+
+
+
+
+
+
+
+
