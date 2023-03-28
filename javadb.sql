f1922d685f0a5872f91ce193fde54eb92bb2e989
seungyeop <rkdtjdduq88@naver.com>
2023-03-28 18:13:31 +0900

    사용자 권한 및 부여

--- javadb.sql
+++ javadb.sql
@@ -1,1 +1,31 @@
-# javadb
+-- javadb
+
+-- userTBL 테이블 생성
+-- no(번호-숫자(4)), username(이름 - 한글(4)), birthYear(년도-숫자(4)), addr(주소-문자(한글,숫자), mobile(010-1234-1234)
+-- no pk 제약조건 지정(제약조건명 pk_userTBL)
+CREATE TABLE userTBL(
+        no number(4) CONSTRAINT pk_userTBL PRIMARY KEY,
+        username nvarchar2(10) NOT NULL,
+        birthYear number(4)NOT NULL,
+        addr nvarchar2(50)NOT NULL,
+        mobile nvarchar2(20)
+        );
+        
+drop table userTBL;
+-- 시퀀스 생성
+-- user_seq 생성(기본)
+CREATE SEQUENCE user_seq;
+
+
+-- insert
+-- no : user_seq 값 넣기
+INSERT INTO userTBL(no, username, birthYear, addr, mobile) 
+VALUES(user_seq.nextval,'홍길동','2010','서울시 종로구 123','010-1234-5678');
+
+commit;
+
+
+
+
+
+
--- system.sql
+++ system.sql
@@ -1,8 +1,22 @@
+-- 사용자 : scott, hr => 데이터베이스 접속하여 데이터를 관리하는 계정
+--         테이블, 인덱스, 뷰 등 여러 객체가 사용자별로 생성
+--         업무에 따라 사용자들을 나눠서 관리
+
+
+-- 스키마 : 데이터간 관계, 데이터 구조, 제약조건 등 데이터를 관리 저장하기 위해 정의한 데이터베이스 구조의 범위
+--         (hr, javadb, scott, system)
+-- 스키마 == 사용자(오라클 데이터베이스 경우)
+
+-- 사용자 생성 : 사용자 생성 권한이 필요함 ==> sys, system
+
+
+
 -- 대소문자 구별 하지 않음(단 비밀번호는 구별함)
 -- CREATE : 생성 / ALTER : 수정 / DROP : 삭제 /DELETE : 삭제
 --
 -- 오라클 버전이 변경되면서 사용자 생성 시 c## 문자를 넣어서 만들도록 변경 됨
 -- JAVADB => C##JAVADB 이런 방식을 C## 사용하지 않겠음
+-- 오라클 버전 올라가면서 사용자 생성시 요구하는 접두사 생략을 위해
 ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 
 -- user 생성은  sys, system 만 가능
@@ -12,6 +26,7 @@ CREATE USER JAVADB IDENTIFIED BY 12345
 DEFAULT TABLESPACE USERS
 TEMPORARY TABLESPACE TEMP;
 
+ALTER USER javadb QUOTA 2M ON USERS;
 -- GRANT : 권한 부여(사용자 생성만 해서는 아무것도 할 수 없음)
 -- JAVADB 에게 CONNECT,RESOURCE 권한을 주겠음
 GRANT CONNECT, RESOURCE TO JAVADB;
@@ -41,7 +56,56 @@ ALTER USER SCOTT TEMPORARY TABLESPACE TEMP;
 
 
 
+-- test 사용자 생성 / 비번 12345
+CREATE USER test IDENTIFIED BY 12345;
 
+-- 접속권한 부여하지 않으면 안됨
+
+-- 권한 권리
+-- 1) 시스템 권한
+--    사용자 생성(CREATE USER) / 수정(ALTER USER) / 삭제(DROP USER)
+--    데이터베이스 접근(CREATE SESSION) / 수정(ALTER SESSION)
+--    여러 객체 생성(view, synonym...) 및 관리 권한
+
+-- 2) 객체 권한
+--    테이블 수정, 삭제, 인덱스 생성, 삽입, 참조, 조회, 수정
+--    뷰 삭제, 삽입, 생성, 조회, 수정
+--    시퀀스 수정, 조회
+--    프로시저, 함수, 패키지 권한
+
+
+-- 권한 부여
+
+-- CREATE SESSION 을 test 에게 부여
+GRANT CREATE SESSION TO test;
+
+GRANT RESOURCE, CREATE TABLE TO test;
+
+-- 테이블 스페이스 USERS에 권한이 없습니다. 권한 주기 위해 아래 SQL문 입력
+ALTER USER test DEFAULT TABLESPACE USERS;
+ALTER USER test TEMPORARY TABLESPACE TEMP;
+ALTER USER test QUOTA 2M ON USERS; -- USERS 에 공간 설정(2MB)
+
+-- scott 에게 test가 생성한 member 테이블 조회 권한 부여
+-- GRANT SELECT ON member TO scott;
+
+-- 권한 취소
+-- REVOKE SELECT, INSERT ON member FROM scott;
+
+-- 롤 : 여러 권한들의 모임
+-- CONNECT 롤 : CREATE SESSION
+-- RESOURCE 롤 : CREATE TRIGGER, CREATE SEQUENCE, CREATE PROCEDURE, CREATE TABLE...
+
+-- 사용자 삭제
+DROP USER test;
+DROP USER test CASCADE; -- test와 관련된 모든 객체 삭제
+
+-- 사용자 생성 + 테이블 스페이스 권한 부여
+CREATE USER test2 IDENTIFIED BY 12345
+DEFAULT TABLESPACE USERS
+TEMPORARY TABLESPACE TEMP;
+-- 권한 부여
+GRANT CONNECT, RESOURCE TO test2;
 
 
 
